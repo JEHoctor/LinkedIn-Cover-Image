de41d31ea7e4e1cf6276d7cf6e88f3151bcb6287
James Hoctor <JEHoctor@protonmail.com>
2024-11-05 14:22:49 -0500

    Fix random GP sampling

--- src/cover_image/pattern_generators.py
+++ src/cover_image/pattern_generators.py
@@ -8,9 +8,11 @@ def random_pattern(sample_points):
 
 
 def gaussian_process_pattern(sample_points, random_state=None):
+    if random_state is None:
+        random_state = np.random.RandomState()
     kernel = 1.0 * gp.kernels.Matern(length_scale=20.0, length_scale_bounds="fixed")
-    gpr = gp.GaussianProcessRegressor(kernel=kernel, random_state=random_state)
-    samples = gpr.sample_y(sample_points).flatten()
+    gpr = gp.GaussianProcessRegressor(kernel=kernel)
+    samples = gpr.sample_y(sample_points, random_state=random_state).flatten()
     samples_min, samples_max = samples.min(), samples.max()
     scaled = (samples - samples_min) / (samples_max - samples_min)
     return scaled
